670e34e3de9b1a3fa036499a7a7ceefb55bb8bae
IT22026170chathuri <135439160+IT22026170chathuri@users.noreply.github.com>
2023-06-04 07:13:47 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,20 @@
 #include <stdio.h>
 
 int main() {
+   int mark1,mark2;avg,total;
+ 
+   cout<<"Enter first subject marks:"<<endl;
+   cin>>mark1;
+   
+   cout<<"Enter second subject marks:"<<endl;
+   cin>>mark2;
+   
+   total = mark1+mark2;
+   avg = total/2;
+   cout<<"Enter total number:",total<<endl;
+   cout<<"The average is :",avg<<endl;
+   
+   
   
   return 0;
 }
