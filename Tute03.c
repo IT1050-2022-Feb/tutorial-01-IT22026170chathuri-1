08afc41f42ee96b02d3ee7296465d0aa99142377
IT22026170chathuri <135439160+IT22026170chathuri@users.noreply.github.com>
2023-06-04 18:00:51 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -11,9 +11,39 @@ sum = 1+2+3+....+ 99+100 = 5050
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
-#include <stdio.h>
+
+  #include <iostream>
+
+
+using namespace std;
+
+
 int main() {
-  
+
+
+  int n, sum=0;
+
+
+  cout << "Enter number :";
+
+
+  cin>>n;
+
+
+  for(int i=1;i<=n;i++)
+  {
+    sum +=i;
+
+
+  }
+
+
+  cout<< "Sum : "<<sum;
+
+
   return 0;
-}
+
+
+} 
+
 
